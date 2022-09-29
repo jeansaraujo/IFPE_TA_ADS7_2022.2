a6e08b1985af7e1a85cf52fa42d6c3a966f0a5e2
João Pedro Monteiro Pereira <joao.pedro@helpper.com.br>
2022-09-29 00:58:29 -0300

    feat: cria controller para Produto com os métodos de salvar, pesquisar, listar e visualizar

---
+++ gestorapp/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produto as Produto;
+
+class ProdutoController extends Controller
+{
+    public function product_new(){
+        $sucesso = false;       
+        return view('produto_novo')->with(compact('sucesso'));        
+    }
+
+    public function product_list(){
+       $produtos = Client::all();   
+        return view('produto_lista')->with(compact('produtos'));        
+    }
+
+    public function product_search(Request $request) {
+        $produtos = DB::table('produtos')
+                    ->where('nome','=',$request->nome)                    
+                    ->get(); 
+        return view('produto_lista')->with(compact('produtos'));        
+    }
+
+    public function product_store(Request $request) {
+       // dd($request);
+        $produto = new Produto();
+        $produto->codigo = $request->codigo;
+        $produto->descricao = $request->descricao;
+        $produto->setor = $request->setor;
+        $produto->fabricante = $request->fabricante;
+        $produto->fornecedor = $request->fornecedor;
+        $produto->precoDeCusto = $request->precoDeCusto;
+        $produto->precoDeVenda = $request->precoDeVenda;
+        $produto->margemDeLucro = $request->margemDeLucro;
+        $produto->estoqueMinimo = $request->estoqueMinimo;
+        $produto->estoqueAtual = $request->estoqueAtual;
+        $produto->unidade = $request->unidade;
+        $produto->ultimoReajuste = $request->ultimoReajuste;
+        $produto->observacoes = $request->observacoes;
+        $produto->save();
+        $sucesso = true;
+        return view('produto_novo')->with(compact("sucesso"));
+    }
+}
